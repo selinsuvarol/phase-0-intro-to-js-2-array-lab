a29c9d4beaf6b77f1c0cc9f8971d7fb520f90434
Selin Su Varol <selinsuvarol@gmail.com>
2022-04-11 14:44:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    return cats;
+}
+function appendCat(name){
+    let newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+function prependCat(name){
+    let newCats = [...cats];
+    newCats.unshift(name);
+    return newCats;
+}
+function removeLastCat(name){
+    let newCats = [...cats];
+    newCats.pop(name);
+    return newCats;
+}
+function removeFirstCat(name){
+    let newCats = [...cats];
+    newCats.shift(name);
+    return newCats;
+}
